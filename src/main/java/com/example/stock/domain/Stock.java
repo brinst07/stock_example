7d77fabca8430712c42c9e2acf76d67c1f1fbfd4
brinst07 <brinst07@gmail.com>
2022-09-13 00:57:47 +0900

    optimisticLock

--- src/main/java/com/example/stock/domain/Stock.java
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -3,10 +3,7 @@ package com.example.stock.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
@@ -16,6 +13,8 @@ public class Stock {
     private Long id;
     private Long productId;
     private Long quantity;
+    @Version
+    private Long version;
 
     public Stock(Long productId, Long quantity) {
         this.productId = productId;
